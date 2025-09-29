bb353c1ef3ebea6eb8e5925d6d1e99543e42380e
sou1burn <koryakinv_04@mail.ru>
2025-09-29 21:28:51 +0300

    logs

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ var (
 
 func main() {
 	fmt.Printf("Lora ESP GSM GPS Project v%s (built %s)\n", version, buildTime)
-	logFile, err := os.OpenFile("build/app.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	logFile, err := os.OpenFile("app.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 	if err != nil {
 		log.Printf("Не удалось открыть файл для логов: %v", err)
 	}
