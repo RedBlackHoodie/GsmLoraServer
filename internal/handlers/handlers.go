95dfa800626c420d3cf8c3621a7cf56b0da2931c
sou1burn <koryakinv_04@mail.ru>
2025-10-01 00:10:08 +0300

    new parsers

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -4,9 +4,11 @@ import (
 	"Lora_Esp_Gsm_Gps_project/internal/core"
 	"Lora_Esp_Gsm_Gps_project/internal/models"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
 	"net"
+	"strconv"
 	"strings"
 	"time"
 
@@ -179,19 +181,19 @@ func ParseClientMessage(h core.MeasurementHandler, message string) (Message, err
 		}
 		return SetSettingsMessage{Params: par}, nil
 
-	} else if strings.HasPrefix(message, "GET_MEASUREMENTS") {
+	} else if strings.HasPrefix(message, "GET_MEASUREMENT") {
 		requestId := 0
 		_, err := fmt.Sscanf(message, "REQUEST_ID=%d", &requestId)
 		if err != nil {
 			log.Printf("error parsing get request: %v", err)
 		}
-		data, err := h.GetMeasurements(requestId)
+		data, err := h.GetMeasurements(int32(requestId))
 		if err != nil {
 			return nil, err
 		}
 
 		return GetDataMessage{Data: data}, nil
-	} else if strings.HasPrefix(message, "START_MEASUREMENTS") {
+	} else if strings.HasPrefix(message, "START_MEASUREMENT") {
 		requestId := 0
 		_, err := fmt.Sscanf(message, "REQUEST_ID=%d", &requestId)
 		if err != nil {
@@ -208,4 +210,42 @@ func ParseClientMessage(h core.MeasurementHandler, message string) (Message, err
 	}
 
 	return UnknownMessageMessage{}, nil
+}
+
+func ParseSession(data string) (models.Session, error) {
+	cleaned := strings.TrimPrefix(data, "ADD_SESSION: ")
+
+	cleaned = strings.Trim(cleaned, "[]")
+
+	parts := strings.Split(cleaned, ",")
+	var session models.Session
+	if len(parts) != 5 {
+		return models.Session{}, errors.New("invalid session format")
+	}
+
+	sessionID, err := strconv.Atoi(strings.TrimSpace(parts[0]))
+	if err != nil {
+		return models.Session{}, errors.New("invalid session format")
+	}
+	session.Id = sessionID
+	session.Name = strings.TrimSpace(parts[1])
+	session.StartTime = strings.TrimSpace(parts[2])
+	session.EndTime = strings.TrimSpace(parts[3])
+	count, err := strconv.Atoi(strings.TrimSpace(parts[4]))
+	if err != nil {
+		return session, fmt.Errorf("invalid points: %w", err)
+	}
+	session.Count = count
+	return session, nil
+}
+
+func ParseRemoveSessionMessage(message string) (int, error) {
+	cleaned := strings.TrimPrefix(message, "REMOVE_SESSION: ")
+
+	sessionID, err := strconv.Atoi(strings.TrimSpace(cleaned))
+	if err != nil {
+		return 0, fmt.Errorf("invalid session ID: %w", err)
+	}
+
+	return sessionID, nil
 }
