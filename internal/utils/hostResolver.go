61e30cdc5fcc371c0113ac25863844a9404b855f
sou1burn <koryakinv_04@mail.ru>
2025-10-05 00:46:13 +0300

    might work

--- internal/utils/hostResolver.go
+++ internal/utils/hostResolver.go
@@ -8,9 +8,11 @@ import (
 	"os/exec"
 	"regexp"
 	"strings"
+	"time"
 )
 
 func ResolveEspHost(hostname, mac string) (string, error) {
+	mac = strings.ToLower(mac)
 	if ip, err := scanARPTable(mac); err == nil {
 		return ip, nil
 	}
@@ -86,9 +88,20 @@ func getNetworkInterface() (*net.Interface, *net.IPNet, error) {
 func activeNetworkScan(iface *net.Interface, ipNet *net.IPNet, targetMAC string) (string, error) {
 	//targetMAC = normalizeMAC(targetMAC)
 
-	_, err := getIPsFromNetwork(ipNet)
+	ips, err := getIPsFromNetwork(ipNet)
 	if err != nil {
 		return "", err
+	}
+	go func() {
+		for i := 0; i < 10 && i < len(ips); i++ {
+			cmd := exec.Command("ping", "-c", "1", "-W", "1", ips[i])
+			cmd.Run()
+		}
+	}()
+	time.Sleep(2 * time.Second)
+
+	if ip, err := scanARPTable(targetMAC); err == nil {
+		return ip, nil
 	}
 
 	cmd := exec.Command("arp-scan", "--interface", iface.Name, "--localnet")
