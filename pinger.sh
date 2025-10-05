0d8a878a3f09d932afc0d654e73f1c00641d84e5
sou1burn <koryakinv_04@mail.ru>
2025-10-06 01:56:05 +0300

    add util: script for arp+ping at esp

--- pinger.sh
+++ pinger.sh
@@ -4,24 +4,25 @@ INTERFACE="wlan0"
 MONITOR_TIME=30
 PCAP_FILE="/tmp/arp_capture.pcap"
 
-echo "arp scan on $INTERFACE for $MONITOR_TIME s..."
+echo "ARP scan on $INTERFACE for $MONITOR_TIME seconds..."
 
-timeout $MONITOR_TIME tcpdump -i $INTERFACE -w $PCAP_FILE 'arp' 2>/dev/null &
+sudo timeout $MONITOR_TIME tcpdump -i $INTERFACE -w $PCAP_FILE 'arp' 2>/dev/null &
 TCPDUMP_PID=$!
 
 wait $TCPDUMP_PID
 
-IP_LIST=$(tcpdump -r $PCAP_FILE 'arp' 2>/dev/null | grep "ARP, Request" | awk -F' ' '{print $7}' | sort -u)
+IP_LIST=$(sudo tcpdump -r $PCAP_FILE 'arp' 2>/dev/null | grep "ARP, Request" | awk -F' ' '{print $7}' | sort -u)
 
 for IP in $IP_LIST; do
-    echo "resolve $IP..."
-    if ! arping -c 2 -I $INTERFACE $IP 2>/dev/null | grep -q "Reply"; then
-        echo "Adress not responding: $IP"
-        echo "Start ping"
+    echo "Testing $IP with ARP..."
+    if ! sudo arping -c 2 -I $INTERFACE $IP 2>/dev/null | grep -q "Reply"; then
+        echo "Address not responding to ARP: $IP"
+        echo "Starting ping to $IP"
         ping $IP
+        sudo rm -f $PCAP_FILE
         exit 0
     fi
 done
 
-echo "done"
-rm -f $PCAP_FILE
+echo "No non-responding ARP addresses found"
+sudo rm -f $PCAP_FILE
